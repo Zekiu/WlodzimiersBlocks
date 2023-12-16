49293a179b93ed87bc33b8de8c409442e237daf7
Zekiu_ <xcof12pl@gmail.com>
2023-12-16 16:05:35 +0100

    Początek

---
+++ src/main/java/xyz/zekiu/wlodzimiers_blocks/WlodzimiersBlocks.java
@@ -0,0 +1,11 @@
+package xyz.zekiu.wlodzimiers_blocks;
+
+import net.fabricmc.api.ModInitializer;
+
+public class WlodzimiersBlocks implements ModInitializer {
+    public static final String MOD_ID = "wlodzimiers_blocks";
+    @Override
+    public void onInitialize() {
+
+    }
+}
