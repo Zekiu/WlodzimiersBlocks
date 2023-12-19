64e25a047115304bced5344cf617cbf480e99ee0
selnov <stepnowskx@wp.pl>
2023-12-19 21:25:10 +0100

    new textures and new variants of kitchen tiles!!!

--- src/main/java/xyz/zekiu/wlodzimiers_blocks/WlodzimiersBlocks.java
+++ src/main/java/xyz/zekiu/wlodzimiers_blocks/WlodzimiersBlocks.java
@@ -16,7 +16,21 @@ public class WlodzimiersBlocks implements ModInitializer {
         PolymerResourcePackUtils.addModAssets(MOD_ID);
         PolymerResourcePackUtils.markAsRequired();
 
-        SimpleCustomBlock.register("kitchen_tiles", BlockModelType.FULL_BLOCK, Blocks.BLACK_CONCRETE);
+        SimpleCustomBlock.register("black_kitchen_tiles", BlockModelType.FULL_BLOCK, Blocks.BLACK_CONCRETE);
+        SimpleCustomBlock.register("blue_kitchen_tiles", BlockModelType.FULL_BLOCK, Blocks.BLUE_CONCRETE);
+        SimpleCustomBlock.register("brown_kitchen_tiles", BlockModelType.FULL_BLOCK, Blocks.BROWN_CONCRETE);
+        SimpleCustomBlock.register("cyan_kitchen_tiles", BlockModelType.FULL_BLOCK, Blocks.CYAN_CONCRETE);
+        SimpleCustomBlock.register("gray_kitchen_tiles", BlockModelType.FULL_BLOCK, Blocks.GRAY_CONCRETE);
+        SimpleCustomBlock.register("green_kitchen_tiles", BlockModelType.FULL_BLOCK, Blocks.GREEN_CONCRETE);
+        SimpleCustomBlock.register("light_blue_kitchen_tiles", BlockModelType.FULL_BLOCK, Blocks.LIGHT_BLUE_CONCRETE);
+        SimpleCustomBlock.register("light_gray_kitchen_tiles", BlockModelType.FULL_BLOCK, Blocks.LIGHT_GRAY_CONCRETE);
+        SimpleCustomBlock.register("lime_kitchen_tiles", BlockModelType.FULL_BLOCK, Blocks.LIME_CONCRETE);
+        SimpleCustomBlock.register("magenta_kitchen_tiles", BlockModelType.FULL_BLOCK, Blocks.MAGENTA_CONCRETE);
+        SimpleCustomBlock.register("orange_kitchen_tiles", BlockModelType.FULL_BLOCK, Blocks.ORANGE_CONCRETE);
+        SimpleCustomBlock.register("pink_kitchen_tiles", BlockModelType.FULL_BLOCK, Blocks.PINK_CONCRETE);
+        SimpleCustomBlock.register("purple_kitchen_tiles", BlockModelType.FULL_BLOCK, Blocks.PURPLE_CONCRETE);
+        SimpleCustomBlock.register("red_kitchen_tiles", BlockModelType.FULL_BLOCK, Blocks.RED_CONCRETE);
+        SimpleCustomBlock.register("yellow_kitchen_tiles", BlockModelType.FULL_BLOCK, Blocks.YELLOW_CONCRETE);
 //      SimpleCustomBlock.register("id_bloku", BlockModelType.KSZTALT_BLOKU, Blocks.WLASCIWOSCI_BLOKU_VANILLA);   ||| DLA PROSTYCH BLOKÓW!!! Pamiętaj aby dodać potrzebne pliki .json w folderze assets oraz data!!!!
 
         LOGGER.info("Wlodzimiers Blocks zostało załadowane miaał");
