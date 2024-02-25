a229a9dd9a7a0079162a265d44e6c83d302af3a6
Zekiu_ <xcof12pl@gmail.com>
2024-02-25 14:35:23 +0100

    last update for < 1.20.4, sad!

--- src/main/java/xyz/zekiu/wlodzimiers_blocks/blocks/SimpleCustomBlock.java
+++ src/main/java/xyz/zekiu/wlodzimiers_blocks/blocks/SimpleCustomBlock.java
@@ -8,12 +8,13 @@ import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import xyz.zekiu.wlodzimiers_blocks.WlodzimiersBlocks;
-import xyz.zekiu.wlodzimiers_blocks.items.SimpleCustomItem;
+import xyz.zekiu.wlodzimiers_blocks.items.SimpleCustomBlockItem;
 
 public class SimpleCustomBlock extends Block implements PolymerTexturedBlock {
     private final BlockState polymerBlockState;
@@ -23,13 +24,34 @@ public class SimpleCustomBlock extends Block implements PolymerTexturedBlock {
         this.polymerBlockState = PolymerBlockResourceUtils.requestBlock(type, PolymerBlockModel.of(new Identifier(WlodzimiersBlocks.MOD_ID, modelId)));
     }
 
-    public static void register(String modelId, BlockModelType type, AbstractBlock abstractBlock) {
+    private static void register(String modelId, BlockModelType type, AbstractBlock abstractBlock) {
         var modId = new Identifier(WlodzimiersBlocks.MOD_ID, modelId);
         var block = Registry.register(Registries.BLOCK, modId,
                 new SimpleCustomBlock(FabricBlockSettings.copy(abstractBlock), type, modelId));
 
-        Registry.register(Registries.ITEM, modId, new SimpleCustomItem(new Item.Settings(), block, modelId));
+        Registry.register(Registries.ITEM, modId, new SimpleCustomBlockItem(new Item.Settings(), block, modelId));
     }
+
+    public static void registerBlocks() {
+        register("black_kitchen_tiles", BlockModelType.FULL_BLOCK, Blocks.BLACK_CONCRETE);
+        register("blue_kitchen_tiles", BlockModelType.FULL_BLOCK, Blocks.BLUE_CONCRETE);
+        register("brown_kitchen_tiles", BlockModelType.FULL_BLOCK, Blocks.BROWN_CONCRETE);
+        register("cyan_kitchen_tiles", BlockModelType.FULL_BLOCK, Blocks.CYAN_CONCRETE);
+        register("gray_kitchen_tiles", BlockModelType.FULL_BLOCK, Blocks.GRAY_CONCRETE);
+        register("green_kitchen_tiles", BlockModelType.FULL_BLOCK, Blocks.GREEN_CONCRETE);
+        register("light_blue_kitchen_tiles", BlockModelType.FULL_BLOCK, Blocks.LIGHT_BLUE_CONCRETE);
+        register("light_gray_kitchen_tiles", BlockModelType.FULL_BLOCK, Blocks.LIGHT_GRAY_CONCRETE);
+        register("lime_kitchen_tiles", BlockModelType.FULL_BLOCK, Blocks.LIME_CONCRETE);
+        register("magenta_kitchen_tiles", BlockModelType.FULL_BLOCK, Blocks.MAGENTA_CONCRETE);
+        register("orange_kitchen_tiles", BlockModelType.FULL_BLOCK, Blocks.ORANGE_CONCRETE);
+        register("pink_kitchen_tiles", BlockModelType.FULL_BLOCK, Blocks.PINK_CONCRETE);
+        register("purple_kitchen_tiles", BlockModelType.FULL_BLOCK, Blocks.PURPLE_CONCRETE);
+        register("red_kitchen_tiles", BlockModelType.FULL_BLOCK, Blocks.RED_CONCRETE);
+        register("yellow_kitchen_tiles", BlockModelType.FULL_BLOCK, Blocks.YELLOW_CONCRETE);
+
+//      register("id_bloku", BlockModelType.KSZTALT_BLOKU, Blocks.WLASCIWOSCI_BLOKU_VANILLA);   ||| DLA PROSTYCH BLOKÓW!!! Pamiętaj aby dodać potrzebne pliki .json w folderze assets oraz data!!!!
+    }
+
     @Override
     public Block getPolymerBlock(BlockState state) {
         return this.polymerBlockState.getBlock();
